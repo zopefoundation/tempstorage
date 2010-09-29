92a65d6f7f2be6d90a8da5f4f634ccb2f16f3efa
Hanno Schlichting <hanno@hannosch.eu>
2010-09-29 12:22:36 +0000

    Prepare tempstorage 2.12.1.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ long_description = file("README.txt").read() + "\n" + \
                    file("CHANGES.txt").read()
 
 setup(name='tempstorage',
-      version = '2.12.1dev',
+      version = '2.12.1',
       url='http://pypi.python.org/pypi/tempstorage',
       license='ZPL 2.1',
       description='A RAM-based storage for ZODB',
